96511dcf435d4122b69bd79dc7f175270dec6c7c
Tarek Bari <baritarek4@gmail.com>
2025-08-13 07:49:15 +0200

    Set up Light & Dark Theme, Shadcn, Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,6 @@ const Home = () => {
   return (
     <div>
       <h1 className=" h1-bold">Welcome to the world of Nextjs</h1>
-      <h1 className=" h1-bold font-inter">
-        Welcome to the world of Nextjs "Inter"
-      </h1>
     </div>
   );
 };
